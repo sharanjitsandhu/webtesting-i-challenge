28d30931f97f5d46dad5d640a528b7d0a04cc8ab
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-13 16:51:47 -0700

    wrote test for repair method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { repair } = require("./enhancer.js");
+
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
+
+test("repair() method", () => {
+  const item = {
+    name: "Valyrian Steel Needle",
+    enhancement: 0,
+    durability: 90
+  };
+  expect(repair(item).durability).toBe(100);
+});
